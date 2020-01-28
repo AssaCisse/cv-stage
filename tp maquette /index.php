16a6059aa17feb4b72186f46a6ab100a379c7c71
Assa Cisse <whay@MacBook-Air-de-Assa.local>
2020-01-28 12:12:03 +0100

    modif position citation

---
+++ tp maquette /index.php
@@ -0,0 +1,12 @@
+<?php 
+   try {
+    $strConnection = 'mysql:host=localhost;dbname=ma_base'; //Ligne 1
+    $arrExtraParam= array(PDO::MYSQL_ATTR_INIT_COMMAND => "SET NAMES utf8"); //Ligne 2
+    $pdo = new PDO($connStr, 'Utilisateur', 'Mot de passe', $arrExtraParam); //Ligne 3; Instancie la connexion
+    $pdo->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);//Ligne 4
+}
+catch(PDOException $e) {
+    $msg = 'ERREUR PDO dans ' . $e->getFile() . ' L.' . $e->getLine() . ' : ' . $e->getMessage();
+    die($msg);
+}
+php>
